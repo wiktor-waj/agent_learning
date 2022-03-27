e29a41c4ebe9954ba8c8177b8791ee3c2a0ff06b
Wiktor Waj <wiktor.waj@gmail.com>
2022-03-27 20:18:47 +0200

    Formating improvements

--- flappy_bird.py
+++ flappy_bird.py
@@ -1,7 +1,8 @@
+# pylint:disable=E1101
 """ Simple flappy bird game implementation """
 import sys
-import pygame
 import random
+import pygame
 
 # from pygame.locals import *
 pygame.init()
@@ -20,9 +21,9 @@ bird_images_path = (
     "assets/img/bird_midflap.png",
     "assets/img/bird_downflap.png",
 )
-pipe_image_path = "assets/img/pipe.png"
-restart_image_path = "assets/img/restart.png"
-restart_image = pygame.image.load(restart_image_path)
+PIPE_IMAGE_PATH = "assets/img/pipe.png"
+RESTART_IMAGE_PATH = "assets/img/restart.png"
+restart_image = pygame.image.load(RESTART_IMAGE_PATH)
 
 # set screen size based on background and base
 screen_width = background_width
@@ -39,27 +40,29 @@ font = pygame.font.SysFont("Fira Mono", 60)
 white = (255, 255, 255)
 
 # define game variables
-base_move = 0
+BASE_MOVE = 0
 MOVE_SPEED = 6  # by how much pixels to move a base in a single frame
 BASE_COLUMN_WIDTH = 24  # width of a single "column" of a base in pixels
-flying = False  # bool for checking flying animation
+FLYING = False  # bool for checking FLYING animation
 game_over = False
-pipe_gap = 150  # the size of gap between top and bottom pipes
-pipe_frequency = 1000  # 1 second between generating new pipes
-last_pipe = pygame.time.get_ticks() - pipe_frequency
+PIPE_GAP = 150  # the size of gap between top and bottom pipes
+PIPE_FREQUENCY = 900  # 900ms between generating new pipes
+last_pipe = pygame.time.get_ticks() - PIPE_FREQUENCY
 score = 0
-was_pipe_passed = False  # variable for tracking score, tracks wheter bird passed a pipe
-debug = False
+WAS_PIPE_PASSED = False  # variable for tracking score, tracks wheter bird passed a pipe
+DEBUG = False
 if "debug" in sys.argv:
-    debug = True
+    DEBUG = True
 
 
 def draw_text(text, font, text_color, x, y):
+    """Function used to draw text on top of the screen"""
     img = font.render(text, True, text_color)
     screen.blit(img, (x, y))
 
 
 def reset_game():
+    """Restarting game variables and flappy bird position"""
     game_over = False
     # clear all the pipes
     pipe_group.empty()
@@ -95,29 +98,26 @@ class Bird(pygame.sprite.Sprite):
 
         # movement handling
         # gravity
-        if flying == True:
+        if FLYING is True:
             self.velocity += 1
             # set a velocity limit
-            if self.velocity > 8:
-                self.velocity = 8
+            self.velocity = min(self.velocity, 8)
             if self.rect.bottom < background_height:
                 self.rect.y += int(self.velocity)
 
-        if game_over == False and flying == True:
-            # flying up
+        if game_over is False and FLYING is True:
+            # FLYING up
             # lmb clicked
-            if pygame.mouse.get_pressed()[0] == 1 and self.lmb_pressed == False:
+            if pygame.mouse.get_pressed()[0] == 1 and self.lmb_pressed is False:
                 self.lmb_pressed = True
                 self.velocity = -10
 
             if pygame.mouse.get_pressed()[0] == 0:
                 self.lmb_pressed = False
 
-            if debug == True:
+            if DEBUG is True:
                 print(
-                    "({}) Current velocity : {}".format(
-                        pygame.time.get_ticks() / 1000, self.velocity
-                    )
+                    f"({pygame.time.get_ticks() / 1000}) Current velocity : {self.velocity}"
                 )
 
             # animation handling
@@ -135,33 +135,39 @@ class Bird(pygame.sprite.Sprite):
                 self.images[self.index], self.velocity * -2
             )
         # "dead" bird effect when game is over
-        elif game_over == True:
+        elif game_over is True:
             self.image = pygame.transform.rotate(self.images[self.index], -90)
         # correct rotation when game has been restarted
-        elif game_over == False and flying == False:
+        elif game_over is False and FLYING is False:
             self.image = pygame.transform.rotate(self.images[self.index], 0)
 
 
 class Pipe(pygame.sprite.Sprite):
+    """Class representing Pipes"""
+
     def __init__(self, x, y, position):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(pipe_image_path)
+        self.image = pygame.image.load(PIPE_IMAGE_PATH)
         self.rect = self.image.get_rect()
         # position 1 is from the top, -1 is from the bottom
         if position == 1:
             # flip the pipe
             self.image = pygame.transform.flip(self.image, False, True)
-            self.rect.bottomleft = [x, y - int(pipe_gap / 2)]
+            self.rect.bottomleft = [x, y - int(PIPE_GAP / 2)]
         if position == -1:
-            self.rect.topleft = [x, y + int(pipe_gap / 2)]
+            self.rect.topleft = [x, y + int(PIPE_GAP / 2)]
 
     def update(self):
+        """Overload of Sprite class Update method used to move pipe to the left
+        and delete it if it goes out of screen"""
         self.rect.x -= MOVE_SPEED
         if self.rect.right < 0:
             self.kill()
 
 
 class Button:
+    """Class representing a button used only by restart button in our case"""
+
     def __init__(self, x, y, image):
         self.image = image
         self.rect = self.image.get_rect()
@@ -178,8 +184,9 @@ class Button:
         )
 
     def is_button_clicked(self):
+        """Predicate determining whether the button has been clicked"""
         action = False
-        if pygame.mouse.get_pressed()[0] == 1 and self.lmb_pressed == False:
+        if pygame.mouse.get_pressed()[0] == 1 and self.lmb_pressed is False:
             action = True
             self.lmp_pressed = True
         elif pygame.mouse.get_pressed()[0] == 0:
@@ -213,21 +220,21 @@ while True:
     pipe_group.draw(screen)
 
     # draw the ground
-    screen.blit(base_image, (base_move, background_height))
+    screen.blit(base_image, (BASE_MOVE, background_height))
 
     # check score
     if len(pipe_group) > 0:  # some pipes have been created
         if (
             bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.left
             and bird_group.sprites()[0].rect.right < pipe_group.sprites()[0].rect.right
-            and was_pipe_passed == False
+            and WAS_PIPE_PASSED is False
         ):  # bird is between top and bottom pipe
-            was_pipe_passed = True
-        if was_pipe_passed == True:
+            WAS_PIPE_PASSED = True
+        if WAS_PIPE_PASSED is True:
             # and if bird has left the area between top and bottom pipe
             if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.right:
                 score += 1
-                was_pipe_passed = False
+                WAS_PIPE_PASSED = False
     draw_text(str(score), font, white, int(screen_width / 2), 30)
 
     # check if any collision occured
@@ -240,13 +247,13 @@ while True:
     # check if the bird hit the ground
     if flappy.rect.bottom >= background_height:
         game_over = True
-        flying = False
+        FLYING = False
 
-    if game_over == False and flying == True:
+    if game_over is False and FLYING is True:
         # generate new pipes
         time_now = pygame.time.get_ticks()
         # enough time has passed to generate a new pipe
-        if time_now - last_pipe > pipe_frequency:
+        if time_now - last_pipe > PIPE_FREQUENCY:
             pipe_height = random.randint(-100, 100)
             bottom_pipe = Pipe(screen_width, int(screen_height / 2) + pipe_height, -1)
             top_pipe = Pipe(screen_width, int(screen_height / 2) + pipe_height, 1)
@@ -255,16 +262,16 @@ while True:
             last_pipe = time_now
 
         # draw and move the base
-        base_move -= MOVE_SPEED
-        if abs(base_move) > BASE_COLUMN_WIDTH:
-            base_move = 0
+        BASE_MOVE -= MOVE_SPEED
+        if abs(BASE_MOVE) > BASE_COLUMN_WIDTH:
+            BASE_MOVE = 0
         # update pipes
         pipe_group.update()
 
-    if game_over == True:
+    if game_over is True:
         restart_button.draw()
-        if restart_button.is_button_clicked() == True:
-            if debug == True:
+        if restart_button.is_button_clicked() is True:
+            if DEBUG is True:
                 print(
                     "({}) Restart button has been clicked!".format(
                         pygame.time.get_ticks() / 1000
@@ -283,10 +290,10 @@ while True:
         # start game
         if (
             event.type == pygame.MOUSEBUTTONDOWN
-            and flying == False
-            and game_over == False
+            and FLYING is False
+            and game_over is False
         ):
-            flying = True
+            FLYING = True
 
     # update everything that has happened to the game
     pygame.display.update()
